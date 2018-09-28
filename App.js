4c7940ae53223f75d201b85f0345c2a8f249e165
Karen Sijbrandij <karen@hackerone.com>
2018-09-28 14:01:21 -0700

    Start work on tabs navigation

--- App.js
+++ App.js
@@ -1,17 +1,53 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
-import AddDeck from './components/AddDeck'
+import { View, StyleSheet, Platform } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducer from './reducers'
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation'
+import { blue, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import AddDeck from './components/AddDeck'
 import DecksList from './components/DecksList'
+
+const Tabs = createBottomTabNavigator({
+  DecksList: {
+    screen: DecksList,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+    }
+  },
+  AddDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'Add deck',
+    }
+  }
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? blue : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : blue,
+      shadowColor: 'rgba(0,0,0,0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={styles.container}>
-          <DecksList />
+          <Tabs />
         </View>
       </Provider>
     );
