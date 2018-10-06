5399abd32a2af5ff2c45e6b93f235590e8f51e50
Karen Sijbrandij <karen@hackerone.com>
2018-10-05 17:30:24 -0700

    Add icons for scorecard + update colors

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import { Text, View, TouchableOpacity, Platform } from 'react-native'
+import { createStackNavigator } from 'react-navigation'
+import { FontAwesome } from '@expo/vector-icons' // meh, smiley, frown-open
+import { red, orange, green, white, purple } from '../utils/colors'
 
 class Quiz extends Component {
   render() {
     return (
       <View>
         <Text>Quiz</Text>
+        <FontAwesome name='frown-o' size={30} color={red} />
+        <FontAwesome name='meh-o' size={30} color={orange} />
+        <FontAwesome name='smile-o' size={30} color={green} />
       </View>
     )
   }
